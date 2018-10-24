0b7074258b904f8dfb5371cff62ba6173a752f7f
ArturoMooreBD <42896075+ArturoMooreBD@users.noreply.github.com>
2018-10-24 12:42:02 -0500

    Update Inserts , update, delete.sql

--- Inserts , update, delete.sql
+++ Inserts , update, delete.sql
@@ -1,181 +1,237 @@
 
-
-
-insert into Cliente
-values('20120618 10:34:09 AM','Arturo',812374465,'San Nicolas','akepirru-6113@yopmail.com')
+Use Ventas
 
 insert into Cliente
-values('20130513 11:20:19 AM','Luis',812334521,'San Pedro','celestino12376@hideweb.xyz')
+values('20120618 10:34:09 AM','Arturo','Moore','Orozco',812374465,'San Nicolas','akepirru-6113@yopmail.com')
 
 insert into Cliente
-values('20150218 09:34:09 AM','Pedro',843274654,'Santa Catarina','bedage@fathir.cf')
+values('20130513 11:20:19 AM','Luis','Lopez','Rodriguez',812334521,'San Pedro','celestino12376@hideweb.xyz')
 
 insert into Cliente
-values('20120614 08:34:09 AM','Cesar',812351465,'San Nicolas','gdfghdfgh@polyswarms.com')
+values('20150218 09:34:09 AM','Pedro','Cruz','Barron',843274654,'Santa Catarina','bedage@fathir.cf')
 
 insert into Cliente
-values('20130112 09:32:02 AM','Samir',812453245,'Escobedo','jghrjgf@bit2tube.com')
+values('20120614 08:34:09 AM','Cesar','Rivera','Cantu',812351465,'San Nicolas','gdfghdfgh@polyswarms.com')
 
 insert into Cliente
-values('20130112 09:32:02 AM','Lola',82435654,'Monterrey','ghdfgs@bit2tube.com')
+values('20130112 09:32:02 AM','Samir','Guillen','Estroncio',812453245,'Escobedo','jghrjgf@bit2tube.com')
 
 insert into Cliente
-values('20130112 06:33:02 AM','Beto',833453452,'Santa Catarina','msdsdfsd@bit2tube.com')
+values('20130113 09:32:02 AM','Lola','Perez','Sanchez',82435654,'Monterrey','ghdfgs@bit2tube.com')
 
 insert into Cliente
-values('20130112 04:12:02 AM','Lupe',83756745,'Escobedo','hjgfjt@bit2tube.com')
+values('20140112 06:33:02 AM','Beto','Milan','Silva',833453452,'Santa Catarina','msdsdfsd@bit2tube.com')
 
 insert into Cliente
-values('20130112 03:15:02 AM','Kiko',823424561,'Escobedo','mfghjtfgjh@bit2tube.com')
+values('20130112 04:12:02 AM','Lupe','Lopez','Mendez',83756745,'Escobedo','hjgfjt@bit2tube.com')
 
-insert into Region
-values('Metropolitana')
+insert into Cliente
+values('20120214 05:55:01 AM','Kiko','Coronado','Torres',823424561,'Escobedo','mfghjtfgjh@bit2tube.com')
 
-insert into Region
-values('Noroeste')
+insert into Cliente
+values('20130113 07:35:12 AM','Ernesto','Hernandez','Almaraz',823424561,'Escobedo','mfghjtfgjh@bit2tube.com')
 
-insert into Region
-values('Noreste')
+insert into Cliente
+values('20130112 10:12:32 AM','Guillermo','Martinez','Mares',823424561,'Escobedo','mfghjtfgjh@bit2tube.com')
 
-insert into Region
-values('Sur')
+insert into Cliente
+values('20151112 11:15:18 AM','Noelia','Escamilla','Toledo',823424561,'Escobedo','mfghjtfgjh@bit2tube.com')
 
-insert into Region
-values('Centro Periferica')
-
-insert into Estado
-values('Cadereyta',1)
-
-insert into Estado
-values('Zaragosa',2)
-
-insert into Estado
-values('Zacatecas',3)
-
-insert into Estado
-values('Parras',4)
-
-insert into Estado
-values('Monterrey',5)
-
-insert into Estado
-values('Zacatecas',6)
-
-insert into Estado
-values('Parras',7)
-
-insert into Estado
-values('Monterrey',8)
-
-insert into Envio_Aerio
-values(12.32,'20120618 10:34:32 AM')
-
-insert into Envio_Aerio
-values(12.32,'20120618 10:34:22 AM')
-
-insert into Envio_Aerio
-values(20.32,'20120618 11:30:02 AM')
-
-insert into Envio_Aerio
-values(40.10,'20120608 08:24:19 AM')
-
-insert into Envio_Aerio
-values(20.10,'20130608 04:24:19 AM')
-
-insert into Envio_Terrestre
-values(50.12,'20120610 05:24:19 AM')
-
-insert into Envio_Terrestre
-values(50.12,'20120610 05:24:19 AM')
-
-insert into Envio_Terrestre
-values(50.12,'20120610 05:24:19 AM')
-
-insert into Envio_Terrestre
-values(50.12,'20120710 05:24:19 AM')
-
-insert into Envio_Terrestre
-values(32.13,'20120410 06:22:19 AM')
 
 insert into Envio
-values(1)
+values('Terrestre','20120410 10:20:59 AM')
 
 insert into Envio
-values(2)
+values('Aereo','20120411 10:10:39 AM')
 
 insert into Envio
-values(3)
+values('Terrestre','20120815 11:30:39 AM')
 
 insert into Envio
-values(4)
+values('Aereo','20120318 08:50:59 AM')
+
+insert into Envio
+values('Terrestre','20120121 09:10:29 AM')
+
+insert into Envio
+values('Aereo','20120602 11:22:09 AM')
+
+insert into Envio
+values('Terrestre','20120510 07:20:19 AM')
+
+insert into Envio
+values('Aereo','20120410 08:26:49 AM')
+
+insert into Envio
+values('Aereo','20120410 11:23:19 AM')
+
+insert into Envio
+values('Terrestre','20120410 06:22:19 AM')
+
+insert into Envio
+values('Aereo','20120410 07:25:19 AM')
+
+insert into Envio
+values('Aereo','20140410 05:23:19 AM')
+
+insert into Envio
+values('Terrestre','20130410 07:22:19 AM')
+
+insert into Envio
+values('Terrestre','20140410 06:22:19 AM')
 
 insert into Empleado
-values('20120410 10:20:19 AM','qwert','gerente','Alberto',814654685)
+values('20120414 11:20:19 AM','qfgdgsd','Ayudante','Andres','Esparza','Hernandez',814632685)
 
 insert into Empleado
-values('20120413 08:25:05 AM','1234dqa','Ayudante General','Estroncio',814434524)
+values('20120413 08:21:05 AM','12d2efs','Ayudante','German','Galileo','Martinez',814464734)
 
 insert into Empleado
-values('20120411 06:12:19 AM','dase1','Ayudante','Gilberto',814345321)
+values('20120410 07:20:19 AM','qwhkjt','Intendente','Alberto','Gonzales','Hernandez',814534535)
 
 insert into Empleado
-values('20120410 07:03:10 AM','asd123','Intendente','Miguel',814654685)
+values('20120413 08:25:05 AM','12ggfbe','Ayudante','Brayan','Martinez','Hebert',814438634)
 
 insert into Empleado
-values('20100620 09:01:39 AM','asd123','Jefe','Adal',814423843)
+values('20120410 06:20:19 AM','qwert','gerente','Alberto','Gonzales','Hernandez',814685123)
+
+insert into Empleado
+values('20120413 08:20:05 AM','1234dqa','Ayudante','Estroncio','Martinez','Hebert',814434524)
+
+insert into Empleado
+values('20120411 06:12:19 AM','dase1','Ayudante','Gilberto','Morales','Acosta',814345321)
+
+insert into Empleado
+values('20120410 07:03:10 AM','asd123','Intendente','Miguel','Barroso','',814654685)
+
+insert into Empleado
+values('20100620 09:01:39 AM','asd123','Jefe','Adal','Salvador','Esquivel',814423843)
+
+insert into Empleado
+values('20100620 07:01:39 AM','asdf3c','MontaCargas','Adal','Salvador','Esquivel',814977643)
 
 insert into Proveedor
-values('Heriberto',46546135,'Escobedo','peripir@bit2tube.com')
+values('Heriberto','Bravo','Lopez',46546135,'Escobedo','peripir@bit2tube.com')
 
 insert into Proveedor
-values('Sergio',83453843,'San Nicolas','tilamo@eos2mail.com')
+values('Sergio','Vargas','Morales',83453843,'San Nicolas','tilamo@eos2mail.com')
 
 insert into Proveedor
-values('Hector',85342443,'Tamaulipas','moyu@bit2tube.com')
+values('Hector','Romero','Govea',85342443,'Tamaulipas','moyu@bit2tube.com')
 
 insert into Proveedor
-values('Laura',23423843,'Santa Catarina','cupuhi@polyswarms.com')
+values('Laura','Del Carmen','Nilo',23423843,'Santa Catarina','cupuhi@polyswarms.com')
 
 insert into Proveedor
-values('Mario',52342343,'Apodaca','bemucawa@spindl-e.com')
+values('Mario','Romero','Rivera',52342343,'Apodaca','bemucawa@spindl-e.com')
 
 insert into Proveedor
-values('Erick',74623423,'Sinaloa','seniwi@spindl-e.com')
+values('Erick','Montero','Galante',74623423,'Garcia', 'seniwi@spindl-e.com')
 
-insert into [Producto_Almacen Stock]
-values('20120410 06:22:19 AM',10)
+insert into Proveedor
+values('Karina','Limas','Macias',14453843,'Santa Catarina','cdfgdfd@polyswarms.com')
 
-insert into [Producto_Almacen Stock]
-values('20120216 06:22:19 AM',15)
+insert into Proveedor
+values('Angel','Almaraz','Castillo',53242343,'Apodaca','bejghfhgj@spindl-e.com')
 
-insert into [Producto_Almacen Stock]
-values('20130610 07:22:19 AM',8)
+insert into Proveedor
+values('Irvin','Montes','Escobar',75603423,'Escobedo','seghfd@spindl-e.com')
 
-insert into [Producto_Almacen Stock]
-values('20150820 04:22:19 AM',5)
+insert into Proveedor
+values('Elizabeth','Lopez','Serna',75603423,'Escobedo','seghfd@spindl-e.com')
 
-insert into [Producto_Almacen Stock]
-values('20120410 06:22:19 AM',5)
-
-insert into Almacen
-values('San pedro',16546843)
-
-insert into Almacen
-values('San pedro',23423443)
-
-insert into Almacen
-values('Guadalupe',45645633)
-
-insert into Almacen
-values('Monterrey',335345434)
-
-insert into Almacen
-values('San nicolas',6457546745)
-
+insert into Proveedor
+values('Daniel','Beltran','Garmendia',812341243,'Veracruz','sdfsdadfa@bit2tube.com')
 
 insert into Producto
-values(14.52,'Chocolate','De fresa','Si')
+values(14.52,'Chocolate','fresa',10,1)
 
+insert into Producto
+values(14.52,'Chocolate','Vainilla',10,1)
+
+insert into Producto
+values(5,'Paleta','Piña',12,2)
+
+insert into Producto
+values(5,'Paleta','Fresa',12,2)
+
+insert into Producto
+values(7,'Chicle','Mango',9,3)
+
+insert into Producto
+values(7,'Chicle','Moras',8,3)
+
+insert into Producto
+values(10,'Caramelo','Sandia',20,4)
+
+insert into Producto
+values(10,'Caramelo','Chile',23,4)
+
+insert into Producto
+values(12,'Gomita','De fresa',25,5)
+
+insert into Producto
+values(12,'Gomita','De fresa',10,5)
+
+insert into Producto
+values(6,'Mazapan','Nuez',12,6)
+
+insert into Producto
+values(6,'Mazapan','fresa',13,6)
+
+insert into Producto
+values(3,'Bolitas','Fresa',14,7)
+
+insert into Producto
+values(3,'Bolitas','Chile',15,7)
+
+insert into Producto
+values(13,'Tamarino','Chamoy',12,8)
+
+insert into Producto
+values(11,'Skittles','Clasico',16,8)
+
+insert into Producto
+values(3,'Lucas','Fresa',18,9)
+
+insert into Producto
+values(3,'Lucas','Chamoy',20,9)
+
+insert into Producto
+values(3,'Hormiguitas','Fresa',18,10)
+
+insert into Producto
+values(12,'Papas','Queso',20,10)
+
+insert into Pedido
+values('20130615 11:20:19 AM',10,10,1,10,1)
+
+insert into Pedido
+values('20110724 09:22:19 AM',12,9,2,9,2)
+
+insert into Pedido
+values('20130804 10:14:19 AM',8,8,3,8,3)
+
+insert into Pedido
+values('20120814 10:24:19 AM',9,7,4,7,4)
+
+insert into Pedido
+values('20121014 09:27:19 AM',7,6,5,6,5)
+
+insert into Pedido
+values('20120214 08:28:19 AM',11,5,6,5,6)
+
+insert into Pedido
+values('20120314 07:30:19 AM',15,4,7,4,7)
+
+insert into Pedido
+values('20140414 06:26:19 AM',6,3,8,3,8)
+
+insert into Pedido
+values('20150414 10:28:19 AM',5,2,9,2,9)
+
+insert into Pedido
+values('20160512 11:20:19 AM',4,1,10,1,10)
 
 UPDATE Cliente
 SET Nombre = 'Johnson'
@@ -212,4 +268,3 @@ WHERE Codigo_Region= 1;
 
 DELETE FROM Estado
 WHERE Codigo_Region= 2;
-
